10f4384a777a637568c1e0721e5a4991b39b9ff7
sirawit2545 <6406021610054@fitm.kmutnb.ac.th>
2022-12-21 11:08:50 +0700

    update

---
+++ if-else.js
@@ -0,0 +1,12 @@
+const status = 200;
+
+if (statup === 200) {
+console. log('OK');
+
+} else if (statup === 400) {
+    console. log('Error!');
+
+} else {
+console. log('Unknown status');
+
+}
---
+++ operator.js
@@ -0,0 +1,8 @@
+let x = 5;
+let y = 2;
+let z= x * y;
+let a= x **y ;
+document. getElementById("demo1").innerHTML = z;
+document. getELementById("demo2"). innerHTML = a;
+
+  
