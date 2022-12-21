0b6b6f00f8155a59a8fa6b52ef79c796f538d150
sirawit2545 <6406021610054@fitm.kmutnb.ac.th>
2022-12-21 10:19:20 +0700

    uodate

---
+++ strtemplitr.js
@@ -0,0 +1,17 @@
+//Concatenation with template literals
+console.log("\n********Concatenation with template literals********\n");
+
+let str1 = "JavaScript";
+let str2 = "fun";
+
+console. log('I am writing code in ${str1}');
+console. log('Formatting in ${stri} is ${str2}!');
+//Express in template lierals
+
+console.log("\n********Expressions in template literals********\n");
+
+let bool1 = true;
+console.log('1+1 is ${1+1}');
+console.log ('the opposite of true is ${!bool1}');
+
+ 
