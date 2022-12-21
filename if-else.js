4d1ad4e622cf4f9152419a8fa3305afc10af4b46
sirawit2545 <6406021610054@fitm.kmutnb.ac.th>
2022-12-21 11:17:26 +0700

    update

--- if-else.js
+++ if-else.js
@@ -1,12 +1,12 @@
 const status = 200;
 
 if (statup === 200) {
-   console. log('OK!');
+   console.log('OK!');
 
 } else if (statup === 400) {
    console. log('Error!');
 
 } else {
-   console. log('Unknown status');
+   console.log('Unknown status');
 
 }
